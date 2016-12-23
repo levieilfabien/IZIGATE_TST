a8f16b4ca420e192a944ddada13553881e302e37
bardouma <bardouma@DK411731.cib.net>
2016-12-23 15:44:57 +0100

    Commit IZIGATE_TST 23/12/ 15:44

---
+++ src/main/bean/CasEssaiIzigateBean.java
@@ -0,0 +1,37 @@
+package main.bean;
+
+import beans.CasEssaiBean;
+
+/**
+ * Classe d'extension des cas d'essai classique pour IZIGATE
+ * @author levieilfa
+ *
+ */
+public class CasEssaiIzigateBean extends CasEssaiBean {
+
+	/**
+	 * Id de sérialisation par défaut.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Le numéro FFI du dossier saisie si il y a lieu.
+	 */
+	private String numeroFFI = "";
+	
+
+	
+	public CasEssaiIzigateBean() {
+		super();
+	}
+
+	public String getNumeroFFI() {
+		return numeroFFI;
+	}
+
+	public void setNumeroFFI(String numeroFFI) {
+		this.numeroFFI = numeroFFI;
+	}
+
+}
+
---
+++ src/main/constantes/Cibles.java
@@ -0,0 +1,62 @@
+package main.constantes;
+
+import beans.CibleBean;
+import constantes.Clefs;
+
+public class Cibles {
+	//////////////////////////////////////////////////////CIBLE A USAGE UNIQUE ///////////////////////////////////////////////////////////////
+	/**
+	 * Zone de saisie de l'identifiant de l'utilisateur
+	 */
+	public static final CibleBean SAISIE_LOGIN =  new CibleBean(Clefs.NAME, "user");
+	
+	/**
+	 * Zone de saisie du mot de passe de l'utilisateur
+	 */
+	public static final CibleBean SAISIE_MDP =  new CibleBean(Clefs.NAME, "mdp");
+	
+	/**TODO Demande de vérification à Fabien
+	 * Bouton de validation des identifiants d'accès à Izigate
+	 */
+	public static final CibleBean BOUTON_VALIDATION_LOGIN =  new CibleBean(Clefs.XPATH, "html/body/table[4]/tbody/tr/td[1]/input");
+	
+	/**
+	 * Zone de saisie du numéro FFI pour la consultation d'un dossier
+	 */
+	public static final CibleBean SAISIE_FFI_CONSULT = new CibleBean(Clefs.NAME, "s1_sioc_id");
+	
+	/**
+	 * Onglet de sélection du distributeur CE
+	 */
+	public static final CibleBean BOUTON_ONGLET_CE = new CibleBean(Clefs.VALEUR, "CE");
+	
+	/**
+	 * Onglet de sélection du distributeur BPOP
+	 */
+	public static final CibleBean BOUTON_ONGLET_BPOP = new CibleBean(Clefs.VALEUR, "BPOP");
+	
+	/**
+	 * Onglet de sélection du distributeur BPCE IOM
+	 */
+	public static final CibleBean BOUTON_ONGLET_BPCEIOM = new CibleBean(Clefs.VALEUR, "BPCE IOM");
+	
+	/**
+	 * Bouton d'accès à la page de mûrissement d'un dossier
+	 */
+	public static final CibleBean BOUTON_MURISSEMENT = new CibleBean(Clefs.VALEUR, "Mûrissement d'un dossier");
+	
+	/**
+	 * Zone de saisie du numéro FFI pour le mûrissement d'un dossier
+	 */
+	public static final CibleBean SAISIE_FFI_MURISSEMENT = new CibleBean(Clefs.NAME, "s2_sioc_id");
+	
+	/**
+	 * Zone de saisie de la date d'instructio d'un dossier pour la recherche d'un dossier
+	 */
+	public static final CibleBean SAISIE_DATE_MURISSEMENT = new CibleBean(Clefs.NAME, "s2_date");
+	
+	/**
+	 * Bouton d'envoi de recherche de dossier
+	 */
+	public static final CibleBean BOUTON_ENVOI_RECHERCHE = new CibleBean(Clefs.VALEUR, "Envoi");
+}
